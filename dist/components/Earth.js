86aaf78e644a36bd8e01695cf9797422013bda38
Ross Leitch <ross@hexhive.io>
2023-08-17 14:18:56 +1200

    Init

--- dist/components/Earth.js
+++ dist/components/Earth.js
@@ -1,5 +1,32 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-const jsx_runtime_1 = require("react/jsx-runtime");
-const SvgEarth = (props) => ((0, jsx_runtime_1.jsxs)("svg", Object.assign({ xmlns: "http://www.w3.org/2000/svg", xmlnsXlink: "http://www.w3.org/1999/xlink", fill: "#fff", fillRule: "evenodd", stroke: "#000", strokeLinecap: "round", strokeLinejoin: "round", fontFamily: "Roboto", fontSize: 14, textAnchor: "middle", viewBox: "-10 -10 41 51" }, props, { children: [(0, jsx_runtime_1.jsx)("style", { children: ".earth_svg__E{stroke-width:.5}" }), (0, jsx_runtime_1.jsx)("use", { xlinkHref: "#earth_svg__A", x: 0.5, y: 0.5 }), (0, jsx_runtime_1.jsx)("symbol", { id: "earth_svg__A", overflow: "visible", children: (0, jsx_runtime_1.jsx)("path", { fill: "none", d: "M10 0v15M5 15h10M7.5 17.5h5M8.75 20h2.5", className: "earth_svg__E" }) })] })));
+const React = __importStar(require("react"));
+const SvgEarth = (props) => (React.createElement("svg", Object.assign({ xmlns: "http://www.w3.org/2000/svg", xmlnsXlink: "http://www.w3.org/1999/xlink", fill: "#fff", fillRule: "evenodd", stroke: "#000", strokeLinecap: "round", strokeLinejoin: "round", fontFamily: "Roboto", fontSize: 14, textAnchor: "middle", viewBox: "-10 -10 41 51" }, props),
+    React.createElement("style", null, ".earth_svg__E{stroke-width:.5}"),
+    React.createElement("use", { xlinkHref: "#earth_svg__A", x: 0.5, y: 0.5 }),
+    React.createElement("symbol", { id: "earth_svg__A", overflow: "visible" },
+        React.createElement("path", { fill: "none", d: "M10 0v15M5 15h10M7.5 17.5h5M8.75 20h2.5", className: "earth_svg__E" }))));
 exports.default = SvgEarth;
