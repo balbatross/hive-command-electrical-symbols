c41122ca35df30d756508e30b35449800b40078d
Ross Leitch <ross@hexhive.io>
2023-09-18 19:49:44 +1200

    DCP

--- dist/components/DCP.js
+++ dist/components/DCP.js
@@ -25,11 +25,11 @@ var __importStar = (this && this.__importStar) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.DCP = void 0;
 const React = __importStar(require("react"));
-const DCP = (props) => (React.createElement("svg", Object.assign({ xmlns: "http://www.w3.org/2000/svg", viewBox: "-5 -5 20 40" }, props),
+const DCP = (props) => (React.createElement("svg", Object.assign({ xmlns: "http://www.w3.org/2000/svg", viewBox: "-5 -5 20 30" }, props),
     React.createElement("circle", { cx: 5, cy: 5, r: 5, fill: "none", stroke: "#000" }),
     React.createElement("path", { stroke: "#000", d: "M5 10v15" })));
 exports.DCP = DCP;
 exports.DCP.metadata = {
     width: 15,
-    height: 22
+    height: 23
 };
--- src/components/DCP.tsx
+++ src/components/DCP.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import type { SVGProps } from "react";
 
 export const DCP = (props: SVGProps<SVGSVGElement>) => (
-  <svg xmlns="http://www.w3.org/2000/svg" viewBox="-5 -5 20 40" {...props}>
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="-5 -5 20 30" {...props}>
     <circle cx={5} cy={5} r={5} fill="none" stroke="#000" />
     <path stroke="#000" d="M5 10v15" />
   </svg>
@@ -10,5 +10,5 @@ export const DCP = (props: SVGProps<SVGSVGElement>) => (
 
 DCP.metadata = {
     width: 15,
-    height: 22
+    height: 23
 }
